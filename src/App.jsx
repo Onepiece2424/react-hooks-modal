4dbfdd01d0920a2f1340fd214d8de78f1b50739f
rioto.nagano <kingdom950927@gmail.com>
2023-01-15 08:55:13 +0900

    ボタンクリック時にモーダルの表示・非表示にする機能の作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css';
-import WelcomeDialog from './components/WelcomeDialog';
+// import WelcomeDialog from './components/WelcomeDialog';
 import useModal from './hooks/useModal';
 
 function App() {
 
-  const { Modal } = useModal();
+  const { Modal, openModal, closeModal } = useModal();
 
   return (
     <div className="App">
       <h1>React Hooksでモーダルウィンドウを作成</h1>
+      <div>
+        <button onClick={openModal}>Open</button>
+      </div>
       <Modal>
         <h2>Content from children</h2>
+        <button onClick={closeModal}>Close</button>
       </Modal>
-      <WelcomeDialog />
+      {/* <WelcomeDialog /> */}
     </div>
   );
 }
--- src/hooks/useModal.jsx
+++ src/hooks/useModal.jsx
@@ -1,10 +1,25 @@
+// カスタムフック
+import { useState } from  'react';
+
 const useModal = () => {
+
+  // モーダルを表示・非表示にするためのstate
+  const [show, setShow] = useState(false);
+
+  const openModal = () => {
+    setShow(true);
+  };
+
+  const closeModal = () => {
+    setShow(false);
+  };
+
   const Modal = ({ children }) => {
-    console.log(children)
+    if (!show) return null;
     return <>{children}</>;
   };
 
-  return { Modal };
+  return { Modal, openModal, closeModal };
 };
 
 export default useModal;
