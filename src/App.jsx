c2255c5279acb1e8b02379f09ee9de849e084787
rioto.nagano <kingdom950927@gmail.com>
2023-01-14 23:34:18 +0900

    useModal Hooksの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import useModal from './hooks/useModal';
 
 function App() {
+  const { Modal } = useModal();
   return (
     <div className="App">
       <h1>React Hooksでモーダルウィンドウを作成</h1>
+      <Modal />
     </div>
   );
 }
